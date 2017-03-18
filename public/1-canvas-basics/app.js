c7d845dc072816567af2663ed5ffcc1a81820b13
Zhengyan Yu <noel.fish6@gmail.com>
2017-03-17 21:33:01 -0400

    completed

--- public/1-canvas-basics/app.js
+++ public/1-canvas-basics/app.js
@@ -1,25 +1,104 @@
 //Part 1: Append a <canvas> element under <div id='plot1'> with the same width and height as the container element
 //Store drawing context as a variable
 //Hint: use canvas.getContext('2d');
+var w = d3.select('.plot').node().clientWidth,
+	h = d3.select('.plot').node().clientHeight;
 
+	// console.log(d3.select('.plot').node().clientWidth);
+	// console.log(d3.select('.plot'));
+
+var canvas1 = d3.select('#plot1').append('canvas')
+				.attr('width',w)
+				.attr('height',h)
+				.node();
+
+//attr is only available in d3.selection
+
+var ctx = canvas1.getContext('2d');
+// console.log(ctx);
 
 //Part 2: Draw a gray background, with fillStyle = 'rgb(250,250,250)'
+ctx.fillStyle = 'rgb(250,250,250)';
+ctx.fillRect(0,0,w,h);
 
 
 //Part 3: Draw a x and y grid, spaced 50px apart, with strokeStyle = 'rgb(180,180,180)'
 //Hint: use context2D.beginPath and context2D path commands within two for... loops
+ctx.strokeStyle = 'rgb(180,180,180)';
+ctx.lineWidth = 0.5;
+ctx.beginPath();
 
+for(var i=0; i<=h; i+=50){
+	ctx.moveTo(0,i);
+	ctx.lineTo(w,i);
+}
+
+for (var j=0;j<=w;j+=50){
+	ctx.moveTo(j,0);
+	ctx.lineTo(j,h);
+}
+
+ctx.stroke();
 
 //Part 4: Draw a filled red rectangle at (50,50), with width = 50 and height = 50
 //Draw a rectangle with red border at (150,50), with width = 50 and height = 50
 //Hint: use context2D.fillRect and context2D.strokeRect
+ctx.fillStyle = 'rgb(255,0,0)';
+ctx.fillRect(50,50,50,50);
+
+ctx.strokeStyle = 'rgb(255,0,0)';
+ctx.strokeRect(150,50,50,50);
+
 
 
 // Part 5: Draw a series of circles and lines using the context2D path commands, as shown
+ctx.beginPath();
+ctx.arc(100,200,50,0,Math.PI*2);
+ctx.save();
+ctx.globalAlpha = .5;
+ctx.fill();
+ctx.restore(); //bring all things back
 
+
+ctx.beginPath();
+ctx.arc(400,200,50,0,Math.PI*2);
+
+ctx.moveTo(750,200);
+ctx.arc(700,200,50,0,Math.PI*2);
+ctx.strokeStyle = 'rgb(255,0,0)';
+ctx.stroke();
 
 // Part 6: Label each circle with coordinates
 // Hint: context2D.fillText
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillText('(100,200)',110,190);
+
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillText('(400,200)',410,190);
+
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillText('(700,200)',710,190);
+
+ctx.beginPath();
+ctx.moveTo(100,200);
+ctx.quadraticCurveTo(250,100,400,200);
+ctx.closePath();
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.moveTo(400,200);
+ctx.quadraticCurveTo(550,300,700,200);
+ctx.closePath();
+ctx.stroke();
+
+
 
 
 // Part 7: append a new <canvas> element under <div id='plot2'>, and copy the content of the first canvas onto it
+var canvas = d3.select('#plot2').append('canvas').attr('width',w).attr('height',h).node();
+
+var ctx2 = canvas.getContext('2d');
+ctx2.drawImage(canvas1,0,0);
+
--- public/2-dispatch/app.js
+++ public/2-dispatch/app.js
@@ -1,52 +1,70 @@
+//Part 1: Append a <canvas> element under <div id='plot1'> with the same width and height as the container element
+//Store drawing context as a variable
+//Hint: use canvas.getContext('2d');
+
 var W = d3.select('#plot1').node().clientWidth,
 	H = d3.select('#plot1').node().clientHeight;
 
-//SVG 1
-var svg1 = d3.select('#plot1').append('svg')
-	.attr('width',W)
-	.attr('height',H)
-	.on('mousemove',function(){
-		var xy = d3.mouse(this); //the x,y coordinates of the mouse in relation to the <svg> element itself
-		d3.select(this).select('.pointer')
-			.attr('cx',xy[0])
-			.attr('cy',xy[1]);
-		//Hint: dispatch.call('mouse:move',this,xy)
-	});
-svg1.append('circle').attr('class','pointer') 
-	.attr('r',5)
-	.style('fill','red');
+var dispatch = d3.dispatch('update');
 
-//Hint: dispatch.on('mouse:move.svg1',function(xy){...})
-//Note the use of .svg1: we add a "name space" to the "mouse:move" event to attach multiple event listeners to the same event
+// //SVG 1
+// var svg1 = d3.select('#plot1').append('svg')
+// 	.attr('width',W)
+// 	.attr('height',H)
+// 	.on('mousemove',function(){
+// 		var xy = d3.mouse(this); //the x,y coordinates of the mouse in relation to the <svg> element itself
+// 		d3.select(this).select('.pointer')
+// 			.attr('cx',xy[0])
+// 			.attr('cy',xy[1]);
+		
+// 		dispatch.call('update',this,xy);
 
-//SVG 2
-var svg2 = d3.select('#plot2').append('svg')
-	.attr('width',W)
-	.attr('height',H)
-	.on('mousemove',function(){
-		var xy = d3.mouse(this); //the x,y coordinates of the mouse in relation to the <svg> element itself
-		d3.select(this).select('.pointer')
-			.attr('cx',xy[0])
-			.attr('cy',xy[1]);
-	});
-svg2.append('circle').attr('class','pointer') 
-	.attr('r',5)
-	.style('fill','red');
+// 	});
+
+// svg1.append('circle').attr('class','pointer') 
+// 	.attr('r',5)
+// 	.style('fill','red');
+
+// dispatch.on('update.svg1',function(xy){return this});
+
+// //Note the use of .svg1: we add a "name space" to the "mouse:move" event to attach multiple event listeners to the same event
+
+// //SVG 2
+// var svg2 = d3.select('#plot2').append('svg')
+// 	.attr('width',W)
+// 	.attr('height',H)
+// 	.on('mousemove',function(){
+// 		var xy = d3.mouse(this); //the x,y coordinates of the mouse in relation to the <svg> element itself
+// 		d3.select(this).select('.pointer')
+// 			.attr('cx',xy[0])
+// 			.attr('cy',xy[1]);
+// 	});
+// svg2.append('circle').attr('class','pointer') 
+// 	.attr('r',5)
+// 	.style('fill','red');
 
 //Can you think of a concise way to make all the dots move in the same way across all .plot elements?
 //Hint below:
-/*d3.selectAll('.plot').append('svg')
+
+d3.selectAll('.plot').append('svg')
 	.attr('width',W)
 	.attr('height',H)
 	.on('mousemove',function(){
 		var xy = d3.mouse(this);
-		dispatch.call('mouse:move',this,xy);
+
+		d3.select(this).select('.pointer')
+			.attr('cx',xy[0])
+			.attr('cy',xy[1]);
+
+		dispatch.call('update',this,xy);
 	})
 	.each(function(){
-		d3.select(this).append('circle')...
+		d3.select(this).append('circle').attr('class','pointer')
+			.attr('r',5)
+			.style('fill', 'red')
 	})
 	.each(function(d,i){
-		dispatch.on('mouse:move.'+i, function(){
-			...
-		})
-	});*/
+		dispatch.on('update.'+i, function(xy){
+		d3.selectAll('.pointer').attr('cx',xy[0]).attr('cy',xy[1])
+		});
+	});
